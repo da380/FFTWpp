727a5ddefdf0c9ca76bd91d789b7d30a6c83ff40
David <da380@cam.ac.uk>
2023-06-28 11:03:42 +0100

    split up some tests

--- test/FFTWTests.cpp
+++ test/FFTWTests.cpp
@@ -7,16 +7,25 @@
 
 TEST(FFTW1DTest, Float) {
   EXPECT_EQ(0, FFTW1DTest<float>());
+}
+
+TEST(FFTW1DTest, FloatNewData) {
   EXPECT_EQ(0, FFTW1DTest<float>(true));
 }
 
 TEST(FFTW1DTest, Double) {
   EXPECT_EQ(0, FFTW1DTest<double>());
+}
+
+TEST(FFTW1DTest, DoubleNewData) {
   EXPECT_EQ(0, FFTW1DTest<double>(true));
 }
 
 TEST(FFTW1DTest, LongDouble) {
   EXPECT_EQ(0, FFTW1DTest<long double>());
+}
+
+TEST(FFTW1DTest, LongDoubleNewData) {
   EXPECT_EQ(0, FFTW1DTest<long double>(true));
 }
 
@@ -25,17 +34,25 @@ TEST(FFTW1DTest, LongDouble) {
 
 TEST(FFTW1DRealTest, Float) {
   EXPECT_EQ(0, FFTW1DRealTest<float>());
+}
+
+TEST(FFTW1DRealTest, FloatNewData) {
   EXPECT_EQ(0, FFTW1DRealTest<float>(true));
 }
 
 TEST(FFTW1DRealTest, Double) {
   EXPECT_EQ(0, FFTW1DRealTest<double>());
+}
+
+TEST(FFTW1DRealTest, DoubleNewData) {
   EXPECT_EQ(0, FFTW1DRealTest<double>(true));
 }
 
-
 TEST(FFTW1DRealTest, LongDouble) {
   EXPECT_EQ(0, FFTW1DRealTest<long double>());
+}
+
+TEST(FFTW1DRealTest, LongDoubleNewData) {
   EXPECT_EQ(0, FFTW1DRealTest<long double>(true));
 }
 
